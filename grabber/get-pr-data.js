821edd017a7ec9045653492f7fad7ef083282359
Tyler Wilding <xtvaser@gmail.com>
2022-04-28 21:52:34 -0400

    grabber: get commits to master as well

--- grabber/get-pr-data.js
+++ grabber/get-pr-data.js
@@ -1,6 +1,4 @@
 // You need a PAT to use this script, it uses GitHub GraphQL
-// Limitations:
-// - this only grabs PR data, not rogue commits to master
 import { Octokit } from "@octokit/rest";
 import * as readline from 'node:readline';
 import { stdin as input, stdout as output } from 'process';
@@ -40,6 +38,9 @@ async function grabPullRequestInfo(client, start_time, end_time, cursor) {
                 login
                 url
               }
+              mergeCommit {
+                oid
+              }
             }
           }
         }
@@ -48,6 +49,44 @@ async function grabPullRequestInfo(client, start_time, end_time, cursor) {
   `,
     {
       "queryInput": `repo:PCSX2/pcsx2 is:pr is:merged merged:${start_time}..${end_time}`,
+      "after": cursor
+    }
+  );
+  console.log(response);
+  return response;
+}
+
+async function grabAllReleases(client, start_time, end_time, cursor) {
+  const response = await client.graphql(
+    `
+    query ($after: String) {
+      repository(owner: "PCSX2", name: "pcsx2") {
+        releases(first: 100, orderBy: {field: CREATED_AT, direction: DESC}, after:$after) {
+          nodes {
+            isDraft
+            publishedAt
+            tagCommit {
+              oid
+              author {
+                user {
+                  login
+                  url
+                }
+              }
+              message
+              url
+            }
+          }
+          pageInfo {
+            startCursor
+            hasNextPage
+            endCursor
+          }
+        }
+      }
+    }
+  `,
+    {
       "after": cursor
     }
   );
@@ -86,10 +125,12 @@ rl.question("GitHub PAT?", (answer) => {
         for (let i = 0; i < resp.search.edges.length; i++) {
           const pr = resp.search.edges[i].node;
           let metadata = {
+            isPR: true,
             url: pr.url,
             title: pr.title,
             createdAt: pr.createdAt,
             mergedAt: pr.mergedAt,
+            sha: pr.mergeCommit.oid,
             labels: [],
             authorName: pr.author == null ? null : pr.author.login,
             authorUrl: pr.author == null ? null : pr.author.url
@@ -103,7 +144,53 @@ rl.question("GitHub PAT?", (answer) => {
           prInfo.push(metadata);
         }
       }
-      fs.writeFileSync(`./out/pr-data-${startTime}-${endTime}.json`, JSON.stringify(prInfo));
+      paginate = true;
+      cursor = null;
+      while (paginate) {
+        const resp = await grabAllReleases(octokit, startTime, endTime, cursor);
+        if (resp.repository.releases.pageInfo.hasNextPage) {
+          cursor = resp.repository.releases.pageInfo.endCursor;
+        } else {
+          paginate = false;
+        }
+        for (let i = 0; i < resp.repository.releases.nodes.length; i++) {
+          const release = resp.repository.releases.nodes[i];
+          // Manually filter
+          if (Date.parse(release.publishedAt) > Date.parse(endTime)) {
+            continue;
+          }
+          if (Date.parse(release.publishedAt) < Date.parse(startTime)) {
+            paginate = false;
+            break;
+          }
+          // Check to see if we already have a PR with this commit
+          let skipEntry = false;
+          for (let j = 0; j < prInfo.length; j++) {
+            const entry = prInfo[j];
+            if (entry.isPR && entry.sha === release.tagCommit.oid) {
+              skipEntry = true;
+              break;
+            }
+          }
+          if (skipEntry) {
+            console.log("Not a commit release! Already have it as a PR! Skippin'");
+            continue;
+          }
+          let metadata = {
+            isPR: false,
+            url: release.tagCommit.url,
+            title: release.tagCommit.message,
+            createdat: release.publishedAt,
+            mergedAt: release.publishedAt,
+            sha: release.tagCommit.oid,
+            labels: ["Commit Release"],
+            authorName: release.tagCommit.author.user == null ? null : release.tagCommit.author.user.login,
+            authorUrl: release.tagCommit.author.user == null ? null : release.tagCommit.author.user.url
+          }
+          prInfo.push(metadata);
+        }
+      }
+      fs.writeFileSync(`./out/pr-data-${startTime}-${endTime}.json`, JSON.stringify(prInfo, null, 2));
       process.exit(0);
     });
   });
